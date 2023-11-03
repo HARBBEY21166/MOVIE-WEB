66e8f2a7102653b5b241188e8aacd5d75bd3cdd1
HARBBEY <112946984+HARBBEY21166@users.noreply.github.com>
2023-11-03 01:00:42 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// script.js
-window.addEventListener('load', () => {
-    const loader = document.querySelector('.loader');
-    
-    setTimeout(() => {
-        loader.style.display = 'none';
-    }, 9000); // Hide the loader after 9 seconds
+//preloader
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  }, 3000); // 9000 milliseconds (9 seconds) delay
 });
+//let see
+
+// script.js
+//window.addEventListener('load', () => {
+    //const loader = document.querySelector('.loader');
+    
+    //setTimeout(() => {
+     //   loader.style.display = 'none';
+   // }, 9000); // Hide the loader after 9 seconds
+//});
 //loader
 
 const product = [
@@ -15,8 +29,85 @@ const product = [
     title: 'Resident evil retribution (2010)',
     link: '#',
   },*/
+  {
+    id: 16,
+    image: 'https://i.pinimg.com/236x/12/42/2d/12422db4398ec5c2537cc0e5f8b96435.jpg',
+    title: 'Gran Turismo (2023)',
+    link: '/movies.html/gran_turismo.html',
+  },
+  
+  {
+    id: 15,
+    image: 'IMG_20231028_020003_542.jpg',
+    title: 'The White Storm 3: Heaven or Hell (2023)',
+    link: '#',
+  },
 
   {
+    id: 14,
+    image: 'IMG_20231028_020024_799.jpg',
+    title: 'Khunpan 3 (2023)',
+    link: '#',
+  },
+
+  {
+    id: 13,
+    image: 'IMG_20231028_020035_336.jpg',
+    title: 'The  Exorcist Believer (2023)',
+    link: '#',
+  },
+
+  {
+    id: 12,
+    image: 'IMG_20231028_020107_188.jpg',
+    title: 'Sayen : Desert Road (2023)',
+    link: '#',
+  },
+
+  {
+    id: 11,
+    image: 'IMG_20231028_020122_606.jpg',
+    title: 'Megalodon : The Frenzy (2023)',
+    link: '#',
+  },
+
+  {
+    id: 10,
+    image: 'IMG_20231028_020132_925.jpg',
+    title: 'Herd (2023)',
+    link: '#',
+  },
+
+{
+    id: 9,
+    image: 'IMG_20231028_020145_641.jpg',
+    title: 'Mortal Kombat Legends : Cage Match (2023)',
+    link: '#',
+  },
+
+  {
+    id: 8,
+    image: 'IMG_20231028_020202_100.jpg',
+    title: 'Expendables 4 (2023)',
+    link: '#',
+  },
+
+  {
+    id: 7,
+    image: 'IMG_20231028_020303_112.jpg',
+    title: 'Muzzle (2023)',
+    link: '#',
+  },
+
+  {
+    id: 6,
+    image: 'IMG_20231028_020312_206.jpg',
+    title: 'Ballerina (2023)',
+    link: '#',
+  },
+
+  
+  /*{
     id: 5,
     image: 'fifth.jpg',
     title: 'Gods Country (2022)',
@@ -56,7 +147,7 @@ const product = [
     image: 'resident.jpg',
     title: 'Resident evil retribution (2010)',
     link: '#',
-  },
+  },*/
         ];
 
 const categories = [...new Set(product.map((item) => { return item }))]
